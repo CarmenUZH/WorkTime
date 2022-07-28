242e1e510402497497f7d96f478f92a6e408f73b
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-07-28 10:16:40 +0200

    Playing around with SQL

---
+++ SQLQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+using System.IO;
+using System.Data.SqlClient;
+
+
+public class SQLQuery
+{
+	public CreateDatabase()
+	{
+		var connectionstring = @"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+		var script = File.ReadAllText(@"C:\Users\carmenkirchdorfer\Documents\GitHub\ALSOVerrechnug\ALSODatabase.sql");
+		SqlConnection conn = new SqlConnection(connectionstring);
+		Server server = new Server(new ServerConnection(conn));
+		server.ConnectionContext.ExecuteNonQuery(script);
+	
+	
+	}
+}
